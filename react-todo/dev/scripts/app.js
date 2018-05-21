6363072d53982ada7c51286de826da4cf15949b8
Shatabdi Bhattacharjee <shatabdi17@gmail.com>
2018-05-21 15:30:18 -0400

    modified styles

--- react-todo/dev/scripts/app.js
+++ react-todo/dev/scripts/app.js
@@ -34,20 +34,18 @@ class App extends React.Component {
     this.handleKeyup = this.handleKeyup.bind(this);
   }
 
-  componentDidMount() {
+componentDidMount() {
     const apiKey='fa46b4109ae577006f5ed95cece9b166855021fc516306c06f350b2323ff70ab';
     const backgroundURL =
       `https://api.unsplash.com/search/photos/?query=flowers&client_id=${apiKey}`;
-      // `https://api.unsplash.com/search/photos/?query=wanderlust&client_id=${apiKey}`;
     
     const randomNum = (max) => Math.floor(Math.random() * max);
     let backgroundResponse;
     axios.get(backgroundURL).then(res => {
-      //console.log(res);
+     
       const randomResults = randomNum(res.data.results.length);
-      //console.log(randomResults);
       backgroundResponse = res.data.results[randomResults].urls.full;
-    })
+})
     
 
     const dbRef = firebase.database().ref('todos');
@@ -77,9 +75,7 @@ class App extends React.Component {
     });
   }
 
-
-
-  handleSubmit(e) {
+handleSubmit(e) {
     e.preventDefault();
   
     const todo = {
@@ -96,20 +92,20 @@ class App extends React.Component {
     this.setState({
       task:''
     });
-  }
-  handleChange(e) {   
+}
+handleChange(e) {   
     this.setState({
       [e.target.name]: e.target.value
     });
     
-  }
+}
 
-  removeTodo(keyToRemove) {
+removeTodo(keyToRemove) {
    
     firebase.database().ref(`todos/${keyToRemove}`).remove();
 }
 
-  completedTodo(keyToUpdate, completed) {
+completedTodo(keyToUpdate, completed) {
     firebase.database().ref(`todos/${keyToUpdate}`)
       .update({
         completed: completed === true ? false : true
@@ -117,14 +113,14 @@ class App extends React.Component {
 }
 
 
-  handleKeyup (e)  {
+handleKeyup (e)  {
     if (e.key === "Enter" && e.target.value.length) {
       this.setState({ 
         name: e.target.value 
       });
       localStorage.setItem("name", e.target.value)
     };
-  } 
+} 
 
     render() {
       return (
@@ -204,22 +200,3 @@ class App extends React.Component {
 }
 
 ReactDOM.render(<App />, document.getElementById('app'));
-
-
-
-// {this.state.todos.length === 0 ? (
-//     <div className="todo-nothing">
-//       <p>There is nothing to do!</p>
-//       <h1>&#x263A;</h1>
-//     </div>
-//   ) : (
-
-// style = {{
-//   background: `url(https://source.unsplash.com/collection/nature${
-//     window.screen.width
-//     }x${window.screen.height}) center center no-repeat fixed`,
-//     backgroundSize: "cover",
-//       height: "100vh",
-//         width: "100vw"
-
-// }}
